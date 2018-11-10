4ab9be6319182f6fed7aa302683672d7803846fa
Matt Johnson <johnsma7@mail.gvsu.edu>
2018-11-10 13:18:17 -0500

    Now with Maven!

---
+++ src/main/java/CentralServer.java
@@ -0,0 +1,57 @@
+
+import com.healthmarketscience.jackcess.*;
+import com.healthmarketscience.jackcess.DatabaseBuilder;
+import com.healthmarketscience.jackcess.Database;
+
+import java.awt.*;
+import java.util.*;
+import java.nio.*;
+import java.net.*;
+import java.io.*;
+
+/*
+    Layout of the gvnapster.mdb file:
+
+    Table SharedFiles has fields:
+        fileID (int)
+        userID (int)
+        name (string)
+        type (string)
+        description (string)
+
+    Table Users has fields:
+        userID (int)
+        first (string)
+        last (string)
+        userName (string)
+        hostName (string)
+        ipAddress (string)
+        port (int)
+ */
+
+public class CentralServer {
+
+
+    public static void main(String[] args){
+        try {
+
+            Database db = DatabaseBuilder.open(new File("E:\\Projects\\Project2\\src\\main\\resources\\gv.mdb"));
+            //Database db = DatabaseBuilder.open(new File("mydb.mdb"));
+            Table users = db.getTable("Users");
+            Table sharedFiles = db.getTable("SharedFiles");
+
+            for (Row row: users
+                 ) {
+                System.out.println("Look ma, a row: " + row);
+            }
+
+            for (Row row: sharedFiles
+                 ) {
+                System.out.println("Different table, row: " + row);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/Host.java
@@ -0,0 +1,9 @@
+
+
+public class Host {
+
+    public static void main(String[] args){
+        FTPClient client = new FTPClient();
+        FTPServer server = new FTPServer();
+    }
+}
