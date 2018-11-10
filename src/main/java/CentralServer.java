a5b38021ad0db3a2c0375571ca60495ec353d184
Matt Johnson <johnsma7@mail.gvsu.edu>
2018-11-10 14:47:44 -0500

    Did some work on the central server. It can read in the xml file and save it to the server's directory.

--- src/main/java/CentralServer.java
+++ src/main/java/CentralServer.java
@@ -2,6 +2,7 @@
 import com.healthmarketscience.jackcess.*;
 import com.healthmarketscience.jackcess.DatabaseBuilder;
 import com.healthmarketscience.jackcess.Database;
+import javax.xml.parsers.*;
 
 import java.awt.*;
 import java.util.*;
@@ -32,26 +33,98 @@ import java.io.*;
 public class CentralServer {
 
 
+
+
     public static void main(String[] args){
         try {
 
-            Database db = DatabaseBuilder.open(new File("E:\\Projects\\Project2\\src\\main\\resources\\gv.mdb"));
-            //Database db = DatabaseBuilder.open(new File("mydb.mdb"));
-            Table users = db.getTable("Users");
-            Table sharedFiles = db.getTable("SharedFiles");
+            int port =12000;
+            ServerSocket welcomeSocket;
+            while(true) {
+                welcomeSocket = new ServerSocket(port);
+                Socket connectionSocket = welcomeSocket.accept();
+                CentralClientHandler cch = new CentralClientHandler(connectionSocket);
+                cch.start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
 
-            for (Row row: users
-                 ) {
+class CentralClientHandler extends Thread{
+    private Database db;
+    private Table users, sharedFiles;
+    private Socket client;
+    private DataOutputStream outToClient;
+    private BufferedReader inFromClient;
+
+    CentralClientHandler(Socket c){
+        try {
+            db = DatabaseBuilder.open(new File("E:\\Projects\\Project2\\src\\main\\resources\\gv.mdb"));//Change this for your individual machines.
+            users = db.getTable("Users");
+            sharedFiles = db.getTable("SharedFiles");
+
+            client = c;
+            outToClient = new DataOutputStream(client.getOutputStream());
+            inFromClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+            /*
+            This is used in debugging:
+
+            for (Row row : users
+            ) {
                 System.out.println("Look ma, a row: " + row);
             }
 
-            for (Row row: sharedFiles
-                 ) {
+            for (Row row : sharedFiles
+            ) {
                 System.out.println("Different table, row: " + row);
-            }
+            }*/
+
 
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run(){
+
+        // Read in the xml file line by line and add the information to the database.
+        // This should only be done the first time that a host connects to the cs.
+        try {
+
+            String nextLine = inFromClient.readLine();
+            File x = new File("fromClient.xml");
+            BufferedWriter bw = new BufferedWriter(new FileWriter(x));
+            while (!nextLine.equals("EOF") || nextLine == null) {
+
+                bw.write(nextLine);
+
+                nextLine = inFromClient.readLine();
+            }
+
+            /*TODO: Parse the xml file and add the relevant data to the database */
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        /*TODO: This is where the cs waits for the keyword search from the host
+            and returns the desired information in a series of strings. */
+
+        while(true){
+            try {
+
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
