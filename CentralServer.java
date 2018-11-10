0fe87037863c4764efa715992b51b4b040b95d9a
Matt Johnson <johnsma7@mail.gvsu.edu>
2018-11-10 11:52:54 -0500

    Files I think we will need for this project.

--- CentralServer.java
+++ CentralServer.java
@@ -1,12 +1,56 @@
+import com.healthmarketscience.jackcess.*;
+import com.healthmarketscience.jackcess.DatabaseBuilder;
+import com.healthmarketscience.jackcess.Database;
+
+import java.awt.*;
 import java.util.*;
 import java.nio.*;
 import java.net.*;
 import java.io.*;
 
-public class CentralServer {
+/*
+    Layout of the gvnapster.mdb file:
 
+    Table SharedFiles has fields:
+        fileID (int)
+        userID (int)
+        name (string)
+        type (string)
+        description (string)
+
+    Table Users has fields:
+        userID (int)
+        first (string)
+        last (string)
+        userName (string)
+        hostName (string)
+        ipAddress (string)
+        port (int)
+ */
+
+public class CentralServer {
+    ;
 
     public static void main(String[] args){
+        try {
+            DatabaseBuilder d = new DatabaseBuilder();
+            Database db = d.open(new File("gvnapster.mdb"));
 
+            Table users = db.getTable("Users");
+            Table sharedFiles = db.getTable("SharedFiles");
+
+            for (Row row: users
+                 ) {
+                System.out.println("Look ma, a row: " + row);
+            }
+
+            for (Row row: sharedFiles
+                 ) {
+                System.out.println("Different table, row: " + row);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
